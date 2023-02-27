0e9e894f462131bfb5becafbbdc89df1e7ec4c8e
WhiteAdam <jurchenko.a@gmail.com>
2023-02-27 12:56:37 +0100

    Burger menu animation

--- tmdb-client/components/Footer.jsx
+++ tmdb-client/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer(props) {
   return (
     <footer className="bg-tmdbDarkBlue">
       <div className="m-auto flex max-w-[var(--maxPrimaryPageWidth)] flex-wrap items-start justify-start gap-10 py-20 pl-10 text-lg text-white md:justify-center  ">
-        <div className="relative flex min-w-[120px] flex-col items-end justify-end gap-4">
+        <div className="relative z-10 flex min-w-[120px] flex-col items-end justify-end gap-4">
           <img
             src="/images/tmdb-logo2.svg"
             alt="logo"
--- tmdb-client/components/burger-menu/BurgerMenu.jsx
+++ tmdb-client/components/burger-menu/BurgerMenu.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import classNames from "classnames";
 import { CrossIcon } from "./CrossIcon";
 import { BurgerIcon } from "./BurgerIcon";
 
 export function BurgerMenu(props) {
   const [isOpen, setIsOpen] = useState(false);
+  const menuClasses = classNames(
+    "absolute top-16 left-0  z-20 h-screen bg-tmdbDarkBlue text-white opacity-90 transition-all overflow-hidden",
+    { "w-4/5": isOpen, "w-0": !isOpen }
+  );
 
   return (
     <div className="flex w-1/3 justify-start sm:hidden">
@@ -11,17 +16,15 @@ export function BurgerMenu(props) {
         <BurgerIcon isShown={!isOpen} callback={() => setIsOpen(!isOpen)} />
         <CrossIcon isShown={isOpen} callback={() => setIsOpen(!isOpen)} />
       </div>
-      {isOpen && (
-        <div className="absolute top-16 left-0 h-screen w-4/5 bg-tmdbDarkBlue p-4 text-white opacity-95">
-          <ol>
-            <li>Some item</li>
-            <li>Some item</li>
-            <li>Some item</li>
-            <li>Some item</li>
-            <li>Some item</li>
-          </ol>
-        </div>
-      )}
+      <div className={menuClasses}>
+        <ol className="m-4">
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+        </ol>
+      </div>
     </div>
   );
 }
