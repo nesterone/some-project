9e0d0968b7bb8688d5a61c403a6424096610a5f2
WhiteAdam <jurchenko.a@gmail.com>
2023-02-28 19:54:03 +0100

    - Change animation property: height -> translate - Code refactoring

--- tmdb-client/components/Header.jsx
+++ tmdb-client/components/Header.jsx
@@ -1,40 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "classnames";
 import { Logo } from "./Logo";
 import { Menu } from "./Menu";
 import { BurgerMenu } from "./burger-menu/BurgerMenu";
 import { useScroll } from "../hooks/useScroll";
-import { BurgerBtn } from "./burger-menu/BurgerBtn";
 
 export function Header(props) {
-  const isHeaderShown = useScroll(64);
-  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const isShown = useScroll(64);
 
   return (
     <header
       className={classNames(
-        "fixed top-0 left-0 z-20 w-full bg-tmdbDarkBlue transition-all duration-300",
-        { "h-16": isHeaderShown, "h-0": !isHeaderShown }
+        "fixed top-0 left-0 z-20 h-16 w-full bg-tmdbDarkBlue transition duration-300",
+        { "translate-y-0": isShown, "-translate-y-full": !isShown }
       )}
     >
-      <BurgerMenu isOpen={isBurgerMenuOpen} />
-      {isHeaderShown && (
-        <div className="mx-auto flex h-full max-w-[var(--maxPrimaryPageWidth)] items-center justify-between px-5 sm:px-10">
-          <div className="flex w-1/3 justify-start sm:hidden">
-            <BurgerBtn
-              isOpen={isBurgerMenuOpen}
-              setIsOpen={setIsBurgerMenuOpen}
-            />
-          </div>
-          <div className="flex w-1/3 items-center justify-center sm:w-auto sm:justify-between">
-            <Logo />
-            <Menu />
-          </div>
-          <div className="flex w-1/3 justify-end text-white sm:w-auto">
-            Some Icons
-          </div>
+      <div className="mx-auto flex h-full max-w-[var(--maxPrimaryPageWidth)] items-center justify-between px-5 sm:px-10">
+        <div className="flex w-1/3 justify-start sm:hidden">
+          <BurgerMenu />
         </div>
-      )}
+        <div className="flex w-1/3 items-center justify-center sm:w-auto sm:justify-between">
+          <Logo />
+          <Menu />
+        </div>
+        <div className="flex w-1/3 justify-end text-white sm:w-auto">
+          Some Icons
+        </div>
+      </div>
     </header>
   );
 }
--- tmdb-client/components/burger-menu/BurgerMenu.jsx
+++ tmdb-client/components/burger-menu/BurgerMenu.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import classNames from "classnames";
+import { BurgerIcon } from "./BurgerIcon";
+import { CrossIcon } from "./CrossIcon";
 
-export function BurgerMenu({ isOpen }) {
+export function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
   const menuClasses = classNames(
-    "absolute top-16 left-0 h-screen bg-tmdbDarkBlue text-white opacity-90 transition-all duration-300 overflow-hidden",
-    { "w-4/5": isOpen, "w-0": !isOpen }
+    "absolute top-16 w-4/5 left-0 h-screen bg-tmdbDarkBlue text-white opacity-90 transition-all duration-300 overflow-hidden",
+    { "translate-x-0": isOpen, "-translate-x-full": !isOpen }
   );
 
   return (
-    <div className={menuClasses}>
-      <ol className="m-4">
-        <li>Some item</li>
-        <li>Some item</li>
-        <li>Some item</li>
-        <li>Some item</li>
-        <li>Some item</li>
-      </ol>
-    </div>
+    <>
+      <div className="flex w-10 items-center justify-center">
+        <BurgerIcon isShown={!isOpen} callback={() => setIsOpen(true)} />
+        <CrossIcon isShown={isOpen} callback={() => setIsOpen(false)} />
+      </div>
+      <div className={menuClasses}>
+        <ol className="m-4">
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+          <li>Some item</li>
+        </ol>
+      </div>
+    </>
   );
 }
